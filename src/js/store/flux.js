228f02347a7c86d508ca9df44a9e57c8b4fd6902
Vero519 <voviedo6@gmail.com>
2024-07-30 23:06:57 +0000

    contact

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,6 +1,8 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			apiUrl: "https://playground.4geeks.com/contact",
+			agenda_slug:"Vero519",
 			demo: [
 				{
 					title: "FIRST",
@@ -12,7 +14,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			contacts: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -37,7 +40,33 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
+			},
+			createAgenda: ()=> {
+				const store = getStore()
+				fetch(`${store.apiUrl}/agenda/${store.agenda_slug}`, {method: "POST"})
+					.then(response => response.json())
+					.then(data => console.log(data))
+					.catch(error => console.log(error))
+			},
+			getContacts: ()=> {
+				const store = getStore()
+				const actions = getActions()
+				fetch(`${store.apiUrl}/agenda/${store.agenda_slug}/contacts`)
+					.then(response => {
+						if(response.status == 404){ 
+							actions.createAgenda()
+							return null
+						}
+						return response.json()
+					})
+					.then(data => {
+						if (data){
+							console.log(data)
+							setStore({contacts: data.contacts})
+						}
+					})
+					.catch(error => console.log(error))
+			},
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,19 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext"
 import "../../styles/home.css";
+import { Card } from "../component/card";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store } = useContext(Context)
+
+	return (
+		<div className="text-center mt-5">
+			{store.contacts && store.contacts.length > 0 && store.contacts.map(item => {
+				return (
+					<Card />
+				)
+			})}
+
+		</div>
+	)
+};
